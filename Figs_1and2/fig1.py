7ee51a7e4b5eb20a28b25e18923efcc4b081915b
Adrian Alva <adrianjosephalva@gmail.com>
2022-01-20 11:33:21 +0530

    Add files via upload

---
+++ Figs_1and2/fig1.py
@@ -0,0 +1,121 @@
+# Program to generate Figure 1.
+
+import numpy
+from matplotlib import pyplot as plt 
+
+# For good plots
+plt.rc('font', family='STIXGeneral', size=14)
+plt.rc('xtick')
+plt.rc('ytick')
+
+# Neuron class
+class FHN:
+	def __init__(self, params):
+		self.eps = params[0]
+		self.a = params[1]
+		self.b = params[2]
+		self.dt = params[3]
+		self.T = params[4]
+		self.A = params[5]
+		self.f = params[6]
+		self.sigma = params[7]
+		self.v0 = params[8]
+		self.w0 = params[9]
+		self.phase = 0.0
+		self.V = numpy.zeros(int(self.T/self.dt))
+		self.W = numpy.zeros(int(self.T/self.dt))
+		self.I = 0.0207
+		self.dT = 0.0
+
+			
+	def rk4(self):
+		time = numpy.arange(0, self.T, self.dt)
+		# Supply input current at t=0.01 s
+		self.feed = (self.I)*numpy.heaviside(time - 0.01, 1)
+		self.V[0] = self.v0
+		self.W[0] = self.w0
+		N = len(self.V)
+
+		# Method for 4th order Runge-Kutta integration.
+		for i in range(N-1):
+			k1 = (1/self.eps)*(self.V[i]*(self.V[i]-self.a)*(1-self.V[i])-self.W[i]+self.feed[i])
+			j1 = (self.V[i]-self.W[i]-self.b)
+
+			k2 = (1/self.eps)*((self.V[i] + self.dt*k1/2)*((self.V[i] + self.dt*k1/2)-self.a)*(1-(self.V[i] + self.dt*k1/2))-(self.W[i] + self.dt*j1/2)+self.feed[i])
+			j2 = ((self.V[i] + self.dt*k1/2)-(self.W[i] + self.dt*j1/2)-self.b)
+
+			k3 = (1/self.eps)*((self.V[i] + self.dt*k2/2)*((self.V[i] + self.dt*k2/2)-self.a)*(1-(self.V[i] + self.dt*k2/2))-(self.W[i] + self.dt*j2/2)+self.feed[i])
+			j3 = ((self.V[i] + self.dt*k2/2)-(self.W[i] + self.dt*j2/2)-self.b)
+
+			k4 = (1/self.eps)*((self.V[i] + self.dt*k3)*((self.V[i] + self.dt*k3)-self.a)*(1-(self.V[i] + self.dt*k3))-(self.W[i] + self.dt*j3)+self.feed[i])
+			j4 = ((self.V[i] + self.dt*k3)-(self.W[i] + self.dt*j3)-self.b)
+
+			self.V[i+1] = self.V[i] + (self.dt/6)*(k1 + 2*k2 + 2*k3 + k4)
+			self.W[i+1] = self.W[i] + (self.dt/6)*(j1 + 2*j2 + 2*j3 + j4)
+
+	
+eps = 0.005
+a = 0.5
+b = 0.15
+dt = 0.0001
+T = 3.0
+A = 0.0
+f = 0.0
+sigma = 0.00
+# Initial conditions set to the stable fixed point.	
+v0 = 0.11151
+w0 = -0.03849
+
+run = FHN([eps, a, b, dt, T, A, f, sigma, v0, w0])
+
+time = numpy.arange(0, T, dt)
+
+x_axis = numpy.arange(-4, 4, 0.01)
+fig, axs = plt.subplots(2, 2)
+axs[0, 0].plot(x_axis, x_axis*(x_axis-a)*(1-x_axis), 'k-', linewidth=1.5)
+axs[0, 0].plot(x_axis, x_axis - b, 'k-', linewidth=1.5)
+axs[0, 0].scatter([0.111522], [-0.038478], marker='.', facecolor='red', s=30)
+axs[0, 0].set_xlabel('V')
+axs[0, 0].set_ylabel('W', labelpad=-5)
+axs[0, 0].set_xlim(-0.2, 1.25)
+axs[0, 0].set_ylim(-0.1, 0.2)
+axs[0, 0].set_title('(a)')
+
+run.I = 0.02
+run.rk4()
+axs[0, 1].plot(x_axis, x_axis*(x_axis-a)*(1-x_axis)+0.02, 'k-', linewidth=1.5)
+axs[0, 1].plot(x_axis, x_axis - b, 'k-', linewidth=1.5)
+axs[0, 1].plot(run.V, run.W, 'r-', linewidth=1.5)
+axs[0, 1].set_xlabel('V')
+axs[0, 1].set_ylabel('W', labelpad=-5)
+axs[0, 1].set_xlim(-0.2, 1.25)
+axs[0, 1].set_ylim(-0.1, 0.2)
+axs[0, 1].set_title('(b)')
+
+run.I = 0.0206662
+run.rk4()
+axs[1, 0].plot(x_axis, x_axis*(x_axis-a)*(1-x_axis)+0.0206662, 'k-', linewidth=1.5)
+axs[1, 0].plot(x_axis, x_axis - b, 'k-', linewidth=1.5)
+axs[1, 0].plot(run.V, run.W, 'r-', linewidth=1.5)
+axs[1, 0].set_xlabel('V')
+axs[1, 0].set_ylabel('W', labelpad=-5)
+axs[1, 0].set_xlim(-0.2, 1.25)
+axs[1, 0].set_ylim(-0.1, 0.2)
+axs[1, 0].set_title('(c)')
+
+run.I = 0.04
+run.rk4()
+axs[1, 1].plot(x_axis, x_axis*(x_axis-a)*(1-x_axis)+0.04, 'k-', linewidth=1.5)
+axs[1, 1].plot(x_axis, x_axis - b, 'k-', linewidth=1.5)
+axs[1, 1].plot(run.V, run.W, 'r-', linewidth=1.5)
+axs[1, 1].set_xlabel('V')
+axs[1, 1].set_ylabel('W', labelpad=-5)
+axs[1, 1].set_xlim(-0.2, 1.25)
+axs[1, 1].set_ylim(-0.1, 0.2)
+axs[1, 1].set_title('(d)')
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=0.7)
+plt.show()
+
+
+
